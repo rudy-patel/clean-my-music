799fb8a0f898175a457f83c98f1dd7e21da8d4fb
Rudy Patel <rutvik@ualberta.ca>
2022-06-03 15:33:38 -0600

    init app

---
+++ Shared/clean_my_musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  clean_my_musicApp.swift
+//  Shared
+//
+//  Created by Rudy Patel on 2022-06-03.
+//
+
+import SwiftUI
+
+@main
+struct clean_my_musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
